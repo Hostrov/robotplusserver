848977fdcee751d54b303a77a39f9f270a789d2a
Hostrov <75016396+Hostrov@users.noreply.github.com>
2023-09-18 15:33:58 +0300

    Add files via upload

---
+++ efrt.py
@@ -0,0 +1,43 @@
+
+#iip = "192.168.209.133"
+#teksock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#ipp = socket.gethostbyname(socket.gethostname())
+
+#teksock.bind((ipp, 6))
+#print('i hear yoiu ')
+#teksock.listen(10)
+#ip, addr = teksock.accept()
+
+#print(ip, addr)
+
+import socket
+import datetime
+import time
+
+ip = socket.gethostbyname(socket.gethostname())
+
+ketofshock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+while True:
+    try:
+        ketofshock.connect((ip, 9090))
+        if ketofshock != None:
+            break
+    except ConnectionRefusedError:
+        print("sapp")
+        continue
+
+while True:
+    curr_ent_time = datetime.datetime.now()
+    timetobesentiguess = curr_ent_time.strftime("%X")
+    pugrade = timetobesentiguess.encode()
+    ketofshock.sendall(pugrade)
+
+
+
+
+
+
+
+
+
